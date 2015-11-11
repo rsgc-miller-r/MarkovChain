22116966597cf7aa50c5a295ebea17fbf6ca8bdc
rsgc-miller-r <rmiller@rsgc.on.ca>
2015-11-11 10:09:28 -0500

    SWEET LAWD
    it works

--- karaethon.playground/Contents.swift
+++ karaethon.playground/Contents.swift
@@ -1,6 +1,7 @@
 
 import Cocoa
 
+
 //file obtained by me
 //transcribed from my copies of 'The Shadow Rising' and 'The Fires of Heaven' by Robert Jordan
 //The Dragon will be reborn
@@ -19,4 +20,18 @@ let cycle = try String(contentsOfURL: file!, encoding:
 let delimiters = NSCharacterSet(charactersInString: ", \n")
 
 let words = cycle.componentsSeparatedByCharactersInSet(delimiters)
+
+var wurdCounts = [String: Int]()
+
+var whyisthisathing = ""
+
+var ahh = 0;
+for ahh in 1...20 {
+    var sweetlord = Int(arc4random_uniform(UInt32(words.count)))
+    whyisthisathing+=words[sweetlord] + " "
+}
+
+
+whyisthisathing
+
 
